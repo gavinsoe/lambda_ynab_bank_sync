daec25e8c2e04704aae1ed75fb53917c157bbd0d
Gavin Soebiantoro <gavin@soebi.com>
2023-05-07 22:01:29 +1000

    created ynab service

---
+++ app/services/models/ynab_models.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+from pydantic import BaseModel
+
+class Budget(BaseModel):
+    id: UUID
+    name: str
+    last_modified_on: datetime
+
+class Account(BaseModel):
+    id: UUID
+    name: str
+    type: str
+    on_budget: bool
+    closed: bool
+    balance: float
+    cleared_balance: float
+    
+class Transaction(BaseModel):
+    id: UUID
+    date: str
+    amount: int
+    payee_id: UUID
+    payee_name: str
+    category_id: Optional[UUID]
+    memo: str
+    cleared: str
+    
+    def __eq__(self, other: object) -> bool:        
+        if not isinstance(other, Transaction):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        # if both have id, then compare that
+        if self.id and other.id and self.id == other.id:
+            return True
+        
+        # otherwise compare fields
+        return self.date == other.date and self.amount == other.amount
---
+++ app/services/ynab_service.py
@@ -0,0 +1,95 @@
+import json
+from typing import List
+
+import requests
+from services.models.ynab_models import Account, Budget, Transaction
+from utilities.config import CFG
+
+
+class YnabService:
+    def __init__(self, access_token):
+        self._endpoint = CFG.endpoints.ynab
+        self._headers = {"Authorisation": "Brearer {access_token}"}
+        
+    def list_budgets(self) -> List[Budget]:
+        try:
+            response = requests.get(
+                headers=self._headers,
+                url=f"{self._endpoint}/budgets"
+            )
+            
+            response.raise_for_status()
+            
+            result = json.loads(response.content.decode("utf-8"))
+            
+            return [Budget(**b) for b in result.get("data",{}).get("budgets",[])]
+        except Exception as e:
+            print(f"Something went wrong while fetching the list of budgets. {e}")
+            raise e
+        
+    def get_accounts(self, budget_id) -> List[Account]:
+        try:
+            response = requests.get(
+                headers=self._headers,
+                url=f"{self._endpoint}/budgets/{budget_id}/accounts"
+            )
+            
+            response.raise_for_status()
+            
+            result = json.loads(response.content.decode("utf-8"))
+            
+            return [Account(**b) for b in result.get("data",{}).get("accounts",[])]
+        except Exception as e:
+            print(f"Something went wrong while fetching the list of accounts. {e}")
+            raise e
+        
+    def get_transactions(self, budget_id, account_id, since_date:str = None) -> List[Transaction]:
+        try:
+            url = f"{self._endpoint}/budgets/{budget_id}/accounts{account_id}/transactions"
+            
+            if since_date:
+                url += f"?since_date={since_date}"
+            
+            response = requests.get(
+                headers=self._headers,
+                url=url
+            )
+            
+            response.raise_for_status()
+            
+            result = json.loads(response.content.decode("utf-8"))
+            
+            return [Transaction(**b) for b in result.get("data",{}).get("transactions",[])]
+        except Exception as e:
+            print(f"Something went wrong while fetching the list of transactions. {e}")
+            raise e
+
+    def create_transactions(self, budget_id, account_id, transactions: List[Transaction]) -> List[Transaction]:
+        try:
+            url = f"{self._endpoint}/budgets/{budget_id}/transactions"
+                        
+            transactions_list = []
+            for t in transactions:
+                t_dict = t.dict()
+                # remove id
+                t_dict.pop("id")
+                t_dict["account_id"] = account_id
+                
+                transactions_list += [t_dict]
+            
+            response = requests.post(
+                headers=self._headers,
+                url=url,
+                json={
+                    "transactions": transactions_list
+                }
+            )
+            
+            response.raise_for_status()
+            
+            result = json.loads(response.content.decode("utf-8"))
+            
+            return [Transaction(**b) for b in result.get("data",{}).get("transactions",[])]
+        except Exception as e:
+            print(f"Something went wrong while importing transactions to ynab. {e}")
+            raise e
