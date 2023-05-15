e389935b34e6cd1796bd73a3070c41a52cd442f5
Gavin Soebiantoro <gavin@soebi.com>
2023-05-15 22:40:56 +1000

    got the lambda running

--- app/main.py
+++ app/main.py
@@ -2,12 +2,13 @@ from sync.sync_factory import SyncFactory
 from utilities.config import CFG
 
 
-def sync():
+def sync(event, context):
+    duration_in_days = event.get("duration_in_days", 2) # defaults to 2 days
     for sync_config in CFG.sync_configs:
         sync_class = SyncFactory.get_sync_class(
             source_config=sync_config.source,
             destination_config=sync_config.destination,
-            duration_in_days=2,
+            duration_in_days=duration_in_days,
         )
         sync_class.run()
 
